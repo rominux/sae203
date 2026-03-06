4379649350b0df49bc2590f5c0f7b8ed677c07fc
Kylian PLANTARD <kylian.plantard.etu@univ-lille.fr>
2026-03-06 23:49:34 +0100

    nettoyage

--- SAE203_Lefebvre_Plantard_Belot/convertdoc.sh
+++ SAE203_Lefebvre_Plantard_Belot/convertdoc.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# On récupère le chemin absolu du dossier .local_gems qui se trouve un niveau plus haut (..)
-export GEM_HOME="$(pwd)/../.local_gems"
-
-# On ajoute les binaires locaux au PATH pour ce script
-export PATH="$GEM_HOME/bin:$PATH"
-
 # Suppression des anciens rendus
 rm -f rapport.html
 rm -f rapport.pdf
 
+if ! command -v asciidoctor &> /dev/null; then
+  echo "Erreur : asciidoctor n'est pas installé."
+  echo "Vous pouvez installer les dépendances avec la commande : gem install asciidoctor asciidoctor-pdf rouge"
+  echo "Ou sur les ordinateurs de l'IUT : gem install --user-install asciidoctor asciidoctor-pdf rouge"
+  exit 1
+fi
+if ! command -v asciidoctor-pdf &> /dev/null; then
+  echo "Erreur : asciidoctor-pdf n'est pas installé."
+  echo "Vous pouvez installer les dépendances avec la commande : gem install asciidoctor asciidoctor-pdf rouge"
+  echo "Ou sur les ordinateurs de l'IUT : gem install --user-install asciidoctor asciidoctor-pdf rouge"
+  exit 1
+fi
+
 # Génération des nouveaux rendus
 asciidoctor rapport.adoc -o rapport.html
 asciidoctor-pdf rapport.adoc -o rapport.pdf
-
-#asciidoctor ../rapport.adoc -o rapport.html
-#asciidoctor-pdf ../rapport.adoc -o rapport.pdf
-
 
 echo "Conversion terminée avec succès via l'environnement local !"
