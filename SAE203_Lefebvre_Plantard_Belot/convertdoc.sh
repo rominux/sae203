56e5fd7ff7542f24e1c048c080c7e31c9ac1c49c
rominux <romain.59.@gmail.com>
2026-03-27 15:13:41 +0100

    test schema

--- SAE203_Lefebvre_Plantard_Belot/convertdoc.sh
+++ SAE203_Lefebvre_Plantard_Belot/convertdoc.sh
@@ -11,13 +11,11 @@ echo "Suppression des anciens rendus..."
 rm -f *.html *.pdf
 
 echo "Génération du Tutoriel..."
-asciidoctor tutoriel.adoc -o SAE203_Tutoriel.html && echo "SAE203_Tutoriel.html terminée"
-asciidoctor-pdf -a pdf-theme=css/mon-theme.yml tutoriel.adoc -o SAE203_Tutoriel.pdf && echo "SAE203_Tutoriel.pdf terminée"
+asciidoctor -r asciidoctor-kroki -a allow-uri-read tutoriel.adoc -o SAE203_Tutoriel.html && echo "SAE203_Tutoriel.html terminée"
+asciidoctor-pdf -r asciidoctor-kroki -a allow-uri-read -a pdf-theme=css/mon-theme.yml tutoriel.adoc -o SAE203_Tutoriel.pdf && echo "SAE203_Tutoriel.pdf terminée"
 
 echo "Génération de la FAQ..."
-asciidoctor faq.adoc -o SAE203_FAQ.html && echo "SAE203_FAQ.html terminée"
-asciidoctor-pdf -a pdf-theme=css/mon-theme.yml faq.adoc -o SAE203_FAQ.pdf && echo "SAE203_FAQ.pdf terminée"
-
-
+asciidoctor -r asciidoctor-kroki -a allow-uri-read faq.adoc -o SAE203_FAQ.html && echo "SAE203_FAQ.html terminée"
+asciidoctor-pdf -r asciidoctor-kroki -a allow-uri-read -a pdf-theme=css/mon-theme.yml faq.adoc -o SAE203_FAQ.pdf && echo "SAE203_FAQ.pdf terminée"
 
 echo "Conversion terminée avec succès ! Vos fichiers finaux sont prêts."
